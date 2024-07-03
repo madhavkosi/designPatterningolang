6abf970dc4d7cb764980eac39e9ba6087c767bad
Madhav Agrawal <madhav.agrawal@colortokens.com>
2024-07-03 23:08:53 +0530

    add data

---
+++ SystemDesign/notes/backofenvelope.md
@@ -0,0 +1,7 @@
+
+**Types of Estimations:**
+- **Load Estimation**: Requests per second, data volume, user traffic.
+- **Storage Estimation**: Amount of storage needed.
+- **Bandwidth Estimation**: Network bandwidth required.
+- **Latency Estimation**: Response time and latency.
+- **Resource Estimation**: Servers, CPUs, memory required.
--- concurrency/main.go
+++ concurrency/main.go
@@ -2,50 +2,30 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-var wg sync.WaitGroup
-var zeroch chan bool
-var oddch chan bool
-var evench chan bool
-
-func zero(n int) {
-	for i := 0; i < n; i++ {
-		fmt.Println(0)
-
-		if i%2 == 0 {
-			oddch <- true
-		} else {
-			evench <- true
+// Function to compare two sets
+func compareSets(set1 map[string][]string, set2 map[string]struct{}) {
+	for value, data := range set1 {
+		if _, found := set2[value]; !found {
+			fmt.Println(data)
+			fmt.Printf("Error: %s Should not be allowed to modify or delete as this is used in %s", value, data)
 		}
-		<-zeroch
-		defer wg.Done()
-	}
-}
-func odd(n int) {
-	for i := 1; i <= n; i += 2 {
-		<-oddch
-		fmt.Println(i)
-		defer wg.Done()
-		zeroch <- true
-	}
-}
-
-func even(n int) {
-	for i := 2; i <= n; i += 2 {
-		<-evench
-		fmt.Println(i)
-		defer wg.Done()
-		zeroch <- true
 	}
 }
 
 func main() {
-	n := 2
-	wg.Add(n * 2)
-	go zero(n)
-	go odd(n)
-	go even(n)
-	wg.Wait()
+	set1 := map[string][]string{
+		"apple":  {"abc", ""},
+		"banana": {},
+		"cherry": {},
+	}
+
+	set2 := map[string]struct{}{
+		"banana": {},
+		"cherry": {},
+		"date":   {},
+	}
+
+	compareSets(set1, set2)
 }
